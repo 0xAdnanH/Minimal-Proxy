cdd44b8fb7ca7b338ff6b9533631ee504135db86
0xAdnanH <0xadnanh@gmail.com>
2023-06-20 16:52:31 +0300

    Update Factory.sol

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
-contract factory {
+/**
+ * @title Factory
+ * @dev A smart contract that facilitates the creation of minimal proxies using the Clones library.
+ */
+contract Factory {
+    /**
+     * @dev Emitted when a new proxy is created.
+     */
     event ProxyCreated();
 
+    /**
+     * @dev Creates a minimal proxy using the Clones library and invokes a function on the created proxy.
+     * @param data The data to be executed on the proxy.
+     * @param implementation The address of the contract to clone.
+     * @param value The value of Ether to send with the call.
+     * @return The address of the created proxy.
+     */
     function minimalProxy(
         bytes memory data,
         address implementation,
